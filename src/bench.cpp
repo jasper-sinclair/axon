9d0e5d4c00d885c30fe84abc0dced6545c2d88e4
jasper-sinclair <jasper.sinclair@mail.com>
2022-11-20 19:40:04 +0100

    Update bench.cpp

--- src/bench.cpp
+++ src/bench.cpp
@@ -8,14 +8,14 @@ uint64_t total_nodes = 0;
 
 uint64_t bench::perft(board& pos, const int depth)
 {
-	uint64_t nodes{0};
+	uint64_t nodes = 0;
 
 	if (depth == 0) return 1;
 
 	const movegen gen(pos, all);
 	const board save(pos);
 
-	for (int i{0}; i < gen.move_cnt; ++i)
+	for (int i = 0; i < gen.move_cnt; ++i)
 	{
 		pos.new_move(gen.movelist[i]);
 		nodes += perft(pos, depth - 1);
@@ -33,7 +33,7 @@ void bench::root_perft(board& pos, const int depth)
 	for (int d = 1; d <= depth; ++d)
 	{
 		const timer time;
-		const uint64_t nodes{perft(pos, d)};
+		const uint64_t nodes = perft(pos, d);
 		total_nodes += nodes;
 		std::cout << "depth " << d << " nodes " << nodes << " time " << time.elapsed()
 			<< " nps " << nodes / (static_cast<uint64_t>(time.elapsed()) + 1) * 1000 << std::endl;
