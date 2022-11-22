c2f326c0a117d3c90ecd5ca5b3d48329e18eb03b
jasper-sinclair <jasper.sinclair@mail.com>
2022-11-23 00:12:27 +0100

    update engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,57 +1,66 @@
 #include <cassert>
-#include "engine.h"
 #include "bitops.h"
+#include "engine.h"
 #include "eval.h"
 #include "game.h"
 #include "hash.h"
 #include "magic.h"
 #include "movegen.h"
-#include "notation.h"
-#include "random.h"
 #include "search.h"
 
-uint16_t engine::alphabeta(board& pos, timemanager& chrono) { return search::idd(pos, chrono); }
+namespace
+{
+	hash table(engine::hash_size);
+}
 
-void engine::init_hash(const int size) { hash::tt_create(size); }
+uint16_t engine::alphabeta(board& pos, timemanager& chrono)
+{
+	stop = false;
+	analysis::reset();
+	return search::id_frame(pos, chrono);
+}
 
-void engine::init_eval() { eval::init_phases(); }
+void engine::init_eval()
+{
+	eval::init();
+}
 
 void engine::init_movegen()
 {
 	movegen::init();
 	magic::init_seeds();
 	magic::init_magic();
-	magic::init_ray(rook);
-	magic::init_ray(bishop);
+	magic::init_ray(rook_slider);
+	magic::init_ray(bishop_slider);
 	magic::init_king();
 	magic::init_knight();
 }
 
-void engine::init_rand() { srand(static_cast<uint16_t>(random_u64_number())); }
+void engine::init_rand()
+{
+	srand(static_cast<uint16_t>(random_u64_number()));
+}
 
 void engine::new_game(board& pos, timemanager& chrono)
 {
-	game::reset();
-	pos.parse_fen(startpos);
-	depth = max_depth;
-	chrono.set_movetime(max_movetime);
-	hash::tt_clear();
+	parse_fen(pos, chrono, startpos);
+}
+
+void engine::new_hash_size(const int size)
+{
+	hash_size = hash::create(size);
 }
 
 void engine::new_move(board& pos, const uint64_t& from_64, const uint64_t& to_64, const uint8_t flag)
 {
-	if (const int control{game::moves / 2 + 1}; game::moves != control)
-		game::game_str += std::to_string(control) + ". ";
+	const int from = lsb(from_64);
+	const int to = lsb(to_64);
 
-	game::game_str += notation::convert_to_san(pos, from_64, to_64, flag) + " ";
-
-	const int from{lsb(from_64)};
-	const int to{lsb(to_64)};
-
-	const uint16_t move{encode(from, to, flag)};
+	const uint16_t move = encode(from, to, flag);
 	pos.new_move(move);
 
 	assert(to_64 & pos.side[pos.turn ^ 1]);
+
 	game::save_move(pos, move);
 }
 
@@ -60,6 +69,6 @@ void engine::parse_fen(board& pos, timemanager& chrono, const std::string& fen)
 	game::reset();
 	depth = max_depth;
 	chrono.set_movetime(max_movetime);
-	hash::tt_clear();
+	hash::clear();
 	pos.parse_fen(fen);
 }
