322e807df7021e1a17f1397f3bbd9fdc557b00c1
jasper sinclair <jasper.sinclair@mail.com>
2022-11-26 23:27:26 +0100

    update game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -1,31 +1,32 @@
 #include "bitops.h"
-#include "movegen.h"
 #include "game.h"
 
 bool draw::by_material(const board& pos)
 {
-	if (lone_bishop(pos) && (!(sqs[white] & pos.pieces[bishops]) || !(sqs[black] & pos.pieces[bishops])))
+	if (lone_bishops(pos) && (!(sqs[white] & pos.pieces[bishop]) || !(sqs[black] & pos.pieces[bishop])))
 		return true;
-	if (lone_knight(pos) && popcnt(pos.pieces[knights]) == 1)
+
+	if (lone_knights(pos) && popcnt(pos.pieces[knight]) == 1)
 		return true;
+
 	return false;
 }
 
-bool draw::by_rep(const board& pos, uint64_t list[], int size)
+bool draw::by_rep(const board& pos, uint64_t list[], const int depth)
 {
-	size -= 1;
-	for (int i{4}; i <= pos.half_moves && i <= size; i += 2)
+	const int size = game::moves + depth - 1;
+	for (int i = 4; i <= pos.half_moves && i <= size; i += 2)
 	{
 		if (list[size - i] == list[size])
 			return true;
 	}
+
 	return false;
 }
 
 void game::reset()
 {
 	moves = 0;
-	game_str.clear();
 	for (auto& m : movelist) m = 0;
 	for (auto& h : hashlist) h = 0;
 }
@@ -37,13 +38,13 @@ void game::save_move(const board& pos, const uint16_t move)
 	moves += 1;
 }
 
-bool draw::verify(const board& pos, uint64_t list[])
+bool draw::verify(const board& pos, uint64_t list[], const int depth)
 {
-	if (pos.half_moves >= 4 && by_rep(pos, list, pos.moves))
+	if (pos.half_moves >= 4 && by_rep(pos, list, depth))
 		return true;
 	if (by_material(pos))
 		return true;
-	if (pos.half_moves >= 50)
+	if (pos.half_moves == 100)
 		return true;
 	return false;
 }
--- src/game.h
+++ src/game.h
@@ -5,26 +5,32 @@
 class game
 {
 public:
-	static int moves;
-	static void reset();
-	static uint16_t movelist[];
-	static uint64_t hashlist[];
-	static std::string game_str;
 	static void save_move(const board& pos, uint16_t move);
+	static void reset();
+	static uint64_t hashlist[];
+	static uint16_t movelist[];
+	static int moves;
 };
 
 namespace draw
 {
 	bool by_material(const board& pos);
-	bool verify(const board& pos, uint64_t list[]);
-	bool by_rep(const board& pos, uint64_t list[], int size);
+	bool by_rep(const board& pos, uint64_t list[], int depth);
+	bool verify(const board& pos, uint64_t list[], int depth);
 }
 
 inline int game::moves;
-inline std::string game::game_str;
 inline uint16_t game::movelist[max_period];
 inline uint64_t game::hashlist[max_period];
 
-inline bool lone_bishop(const board& pos) { return (pos.pieces[bishops] | pos.pieces[kings]) == pos.side[both]; }
-inline bool lone_knight(const board& pos) { return (pos.pieces[knights] | pos.pieces[kings]) == pos.side[both]; }
-constexpr uint64_t sqs[] { 0xaa55aa55aa55aa55, 0x55aa55aa55aa55aa };
+inline bool lone_bishops(const board& pos)
+{
+	return (pos.pieces[bishop] | pos.pieces[king]) == pos.side[both];
+}
+
+inline bool lone_knights(const board& pos)
+{
+	return (pos.pieces[knight] | pos.pieces[king]) == pos.side[both];
+}
+
+constexpr uint64_t sqs[]{0xaa55aa55aa55aa55, 0x55aa55aa55aa55aa};
