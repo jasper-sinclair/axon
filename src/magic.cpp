0cd81edc910f04fc1683270e541288f84112a0ba
Jasper Sinclair <jasper.sinclair@mail.com>
2022-11-29 17:33:00 +0100

    optimize magic.cpp

--- src/magic.cpp
+++ src/magic.cpp
@@ -1,27 +1,37 @@
-#include <algorithm>
 #include <cassert>
 #include <random>
 #include "bitops.h"
+#include "board.h"
+#include "common.h"
 #include "magic.h"
 #include "movegen.h"
 #include "random.h"
 
-void magic::init_blocker(const uint8_t sl, std::vector<uint64_t>& blocker)
+uint64_t attack::by_pawns(const board& pos, const int col)
 {
+	assert(col == white || col == black);
+
+	uint64_t att_table = shift(pos.pieces[pawn] & pos.side[col] & ~border[col], cap_left[col]);
+	att_table |= shift(pos.pieces[pawn] & pos.side[col] & ~border[col ^ 1], cap_right[col]);
+
+	return att_table;
+}
+
+void magic::init_blocker(const int sl, std::vector<uint64_t>& blocker)
+{
+	assert(sl == rook_slider || sl == bishop_slider);
+	assert(blocker.empty() || blocker.size() == 102400U);
+
 	bool bit[12]{false};
 
-	assert(blocker.size() == 0U || blocker.size() == 102400U);
-
-	for (int sq{0}; sq < 64; ++sq)
+	for (int sq = 0; sq < 64; ++sq)
 	{
-		assert(slider[sl][sq].offset == 0);
-
 		slider[sl][sq].offset = blocker.size();
 
-		uint64_t mask_split[12]{};
-		int bits_in{0};
+		uint64_t mask_split[12]{0};
+		int bits_in = 0;
 
-		uint64_t mask_bit{slider[sl][sq].mask};
+		uint64_t mask_bit = slider[sl][sq].mask;
 		while (mask_bit)
 		{
 			mask_split[bits_in++] = 1ULL << lsb(mask_bit);
@@ -30,15 +40,14 @@ void magic::init_blocker(const uint8_t sl, std::vector<uint64_t>& blocker)
 		assert(bits_in <= 12);
 		assert(bits_in >= 5);
 		assert(popcnt(slider[sl][sq].mask) == bits_in);
-		assert(slider[sl][sq].shift == 0);
 
 		slider[sl][sq].shift = 64 - bits_in;
 
-		const int max{1 << bits_in};
-		for (int a{0}; a < max; ++a)
+		const int max = 1 << bits_in;
+		for (int a = 0; a < max; ++a)
 		{
-			uint64_t pos{0};
-			for (int b{0}; b < bits_in; ++b)
+			uint64_t pos = 0;
+			for (int b = 0; b < bits_in; ++b)
 			{
 				if (!(a % (1U << b)))
 					bit[b] = !bit[b];
@@ -50,42 +59,40 @@ void magic::init_blocker(const uint8_t sl, std::vector<uint64_t>& blocker)
 	}
 }
 
-void magic::init_connect(const uint8_t sl, const std::vector<uint64_t>& blocker, const std::vector<uint64_t>& attack_temp)
+void magic::init_connect(const int sl, const std::vector<uint64_t>& blocker, const std::vector<uint64_t>& attack_temp)
 {
-	assert(sl == rook || sl == bishop);
-	assert(attack_table.size() == 0U || attack_table.size() == 102400U);
+	assert(sl == rook_slider || sl == bishop_slider);
+	assert(attack_table.empty() || attack_table.size() == 102400U);
 
-	for (int sq{0}; sq < 64; ++sq)
+	for (int sq = 0; sq < 64; ++sq)
 	{
-		const int max{1 << (64 - slider[sl][sq].shift)};
+		const int max = 1 << (64 - slider[sl][sq].shift);
 
-		for (int cnt{0}; cnt < max; ++cnt)
-			attack_table[slider[sl][sq].offset
-				+ static_cast<int>(blocker[slider[sl][sq].offset + cnt] * slider[sl][sq].magic
-					>> slider[sl][sq].shift)] = attack_temp[slider[sl][sq].offset + cnt];
+		for (int cnt = 0; cnt < max; ++cnt)
+		{
+			attack_table[slider[sl][sq].offset + (blocker[slider[sl][sq].offset + cnt] * slider[sl][sq].magic >> slider[sl][sq].shift)]
+				= attack_temp[slider[sl][sq].offset + cnt];
+		}
 	}
 }
 
 void magic::init_king()
 {
-	for (int i{0}; i < 64; ++i)
+	for (int sq = 0; sq < 64; ++sq)
 	{
-		const uint64_t sq{1ULL << i};
-		assert(king_table[sq] == 0ULL);
+		const uint64_t sq64 = 1ULL << sq;
 
-		for (int dir{0}; dir < 8; ++dir)
+		for (const auto dir : ray)
 		{
-			if (const uint64_t att{sq}; !(att & ray[dir].border))
-			{
-				king_table[i] |= shift(att, ray[dir].shift);
-			}
+			if (const uint64_t att = sq64; !(att & dir.border))
+				movegen::king_table[sq] |= shift(att, dir.shift);
 		}
 	}
 }
 
 void magic::init_knight()
 {
-	constexpr rays pattern[]
+	constexpr pattern jump[]
 	{
 		{15, 0xffff010101010101}, {6, 0xff03030303030303},
 		{54, 0x03030303030303ff}, {47, 0x010101010101ffff},
@@ -93,17 +100,14 @@ void magic::init_knight()
 		{10, 0xffc0c0c0c0c0c0c0}, {17, 0xffff808080808080}
 	};
 
-	for (int i{0}; i < 64; ++i)
+	for (int sq = 0; sq < 64; ++sq)
 	{
-		const uint64_t sq{1ULL << i};
-		assert(knight_table[sq] == 0ULL);
+		const uint64_t sq64 = 1ULL << sq;
 
-		for (int dir{0}; dir < 8; ++dir)
+		for (const auto dir : jump)
 		{
-			if (const uint64_t att{sq}; !(att & pattern[dir].border))
-			{
-				knight_table[i] |= shift(att, pattern[dir].shift);
-			}
+			if (const uint64_t att = sq64; !(att & dir.border))
+				movegen::knight_table[sq] |= shift(att, dir.shift);
 		}
 	}
 }
@@ -119,7 +123,7 @@ void magic::init_magic()
 	std::vector<uint64_t> blocker;
 	blocker.reserve(table_size);
 
-	for (uint8_t sl{rook}; sl <= bishop; ++sl)
+	for (int sl = rook_slider; sl <= bishop_slider; ++sl)
 	{
 		init_mask(sl);
 		init_blocker(sl, blocker);
@@ -129,25 +133,23 @@ void magic::init_magic()
 	}
 }
 
-void magic::init_magic(const uint8_t sl, const std::vector<uint64_t>& blocker, const std::vector<uint64_t>& attack_temp)
+void magic::init_magic(const int sl, const std::vector<uint64_t>& blocker, const std::vector<uint64_t>& attack_temp)
 {
 	bool fail;
-	for (int sq{0}; sq < 64; ++sq)
+	for (int sq = 0; sq < 64; ++sq)
 	{
 		std::vector<uint64_t> occ;
 
-		const int size_occ{1 << (64 - slider[sl][sq].shift)};
-		occ.resize(size_occ);
+		const int occ_size = 1 << (64 - slider[sl][sq].shift);
+		occ.resize(occ_size);
+
 		assert(occ.size() <= 4096U);
+		assert(sq / 4 <= 16 && sq / 4 >= 0);
 
-		assert(static_cast<int>(sq / 4) <= 16);
-		assert(static_cast<int>(sq / 4) >= 0);
+		rand_xor rand_gen{seeds[sq / 4]};
 
-		rand_xor rand_gen{seeds[(sq / 4)]};
+		const int max = 1 << (64 - slider[sl][sq].shift);
 
-		const int max{1 << (64 - slider[sl][sq].shift)};
-
-		assert(slider[sl][sq].magic == 0ULL);
 		do
 		{
 			do
@@ -158,13 +160,16 @@ void magic::init_magic(const uint8_t sl, const std::vector<uint64_t>& blocker, c
 
 			fail = false;
 			occ.clear();
-			occ.resize(size_occ);
+			occ.resize(occ_size);
 
-			for (int i{0}; !fail && i < max; ++i)
+			for (int i = 0; !fail && i < max; ++i)
 			{
-				if (const int index{static_cast<int>(blocker[slider[sl][sq].offset + i] * slider[sl][sq].magic >> slider[sl][sq].shift)}; !occ[index])
-					occ[index] = attack_temp[slider[sl][sq].offset + i];
-				else if (occ[index] != attack_temp[slider[sl][sq].offset + i])
+				const auto idx = blocker[slider[sl][sq].offset + i] * slider[sl][sq].magic >> slider[sl][sq].shift;
+				assert(idx <= static_cast<uint64_t>(occ_size));
+
+				if (!occ[idx])
+					occ[idx] = attack_temp[slider[sl][sq].offset + i];
+				else if (occ[idx] != attack_temp[slider[sl][sq].offset + i])
 				{
 					fail = true;
 					break;
@@ -175,48 +180,47 @@ void magic::init_magic(const uint8_t sl, const std::vector<uint64_t>& blocker, c
 	}
 }
 
-void magic::init_mask(const uint8_t sl)
+void magic::init_mask(const int sl)
 {
-	assert(sl == rook || sl == bishop);
+	assert(sl == rook_slider || sl == bishop_slider);
 
-	for (int i{0}; i < 64; ++i)
+	for (int sq = 0; sq < 64; ++sq)
 	{
-		const uint64_t sq{1ULL << i};
-		assert(slider[sl][sq].mask == 0ULL);
+		const uint64_t sq64 = 1ULL << sq;
 
-		for (int dir{sl}; dir < 8; dir += 2)
+		for (int dir = sl; dir < 8; dir += 2)
 		{
-			uint64_t flood{sq};
+			uint64_t flood = sq64;
 			while (!(flood & ray[dir].border))
 			{
-				slider[sl][i].mask |= flood;
-				shift_real(flood, ray[dir].shift);
+				slider[sl][sq].mask |= flood;
+				real_shift(flood, ray[dir].shift);
 			}
 		}
-		slider[sl][i].mask ^= sq;
+		slider[sl][sq].mask ^= sq64;
 	}
 }
 
-void magic::init_move(const uint8_t sl, const std::vector<uint64_t>& blocker, std::vector<uint64_t>& attack_temp)
+void magic::init_move(const int sl, const std::vector<uint64_t>& blocker, std::vector<uint64_t>& attack_temp)
 {
-	assert(sl == rook || sl == bishop);
-	assert(attack_temp.size() == 0U || attack_temp.size() == 102400U);
+	assert(sl == rook_slider || sl == bishop_slider);
+	assert(attack_temp.empty() || attack_temp.size() == 102400U);
 
-	for (int i{0}; i < 64; ++i)
+	for (int sq = 0; sq < 64; ++sq)
 	{
-		const uint64_t sq{1ULL << i};
+		const uint64_t sq64 = 1ULL << sq;
 
-		const int max{1 << (64 - slider[sl][i].shift)};
-		for (int cnt{0}; cnt < max; ++cnt)
+		const int max = 1 << (64 - slider[sl][sq].shift);
+		for (int cnt = 0; cnt < max; ++cnt)
 		{
-			uint64_t pos{0};
+			uint64_t pos = 0;
 
-			for (int dir{sl}; dir < 8; dir += 2)
+			for (int dir = sl; dir < 8; dir += 2)
 			{
-				uint64_t flood{sq};
-				while (!(flood & ray[dir].border) && !(flood & blocker[slider[sl][i].offset + cnt]))
+				uint64_t flood = sq64;
+				while (!(flood & ray[dir].border) && !(flood & blocker[slider[sl][sq].offset + cnt]))
 				{
-					shift_real(flood, ray[dir].shift);
+					real_shift(flood, ray[dir].shift);
 					pos |= flood;
 				}
 			}
@@ -227,22 +231,21 @@ void magic::init_move(const uint8_t sl, const std::vector<uint64_t>& blocker, st
 	}
 }
 
-void magic::init_ray(const uint8_t sl)
+void magic::init_ray(const int sl)
 {
-	assert(sl == rook || sl == bishop);
+	assert(sl == rook_slider || sl == bishop_slider);
 
-	for (int i{0}; i < 64; ++i)
+	for (int sq = 0; sq < 64; ++sq)
 	{
-		const uint64_t sq{1ULL << i};
-		assert(slide_ray[sl][sq] == 0ULL);
+		const uint64_t sq64 = 1ULL << sq;
 
-		for (int dir{sl}; dir < 8; dir += 2)
+		for (int dir = sl; dir < 8; dir += 2)
 		{
-			uint64_t flood{sq};
+			uint64_t flood = sq64;
 			while (!(flood & ray[dir].border))
 			{
-				shift_real(flood, ray[dir].shift);
-				slide_ray[sl][i] |= flood;
+				real_shift(flood, ray[dir].shift);
+				slide_ray[sl][sq] |= flood;
 			}
 		}
 	}
--- src/magic.h
+++ src/magic.h
@@ -1,28 +1,77 @@
 #pragma once
-#include <cstdint>
-#include <vector>
+#include "common.h"
+#include "movegen.h"
 
 namespace magic
 {
-	struct magics
+	struct entry
 	{
-		size_t offset;
-		uint64_t mask;
-		uint64_t magic;
 		int shift;
+		size_t offset;
+		uint64_t magic;
+		uint64_t mask;
+	};
+
+	struct pattern
+	{
+		int shift;
+		uint64_t border;
 	};
 
 	inline uint64_t seeds[16];
-	inline magics slider[2][64];
-	void init_seeds();
-	void init_magic();
+	void init_blocker(int sl, std::vector<uint64_t>& blocker);
+	void init_connect(int sl, const std::vector<uint64_t>& blocker, const std::vector<uint64_t>& attack_temp);
 	void init_king();
 	void init_knight();
-	void init_ray(uint8_t sl);
-	void init_mask(uint8_t sl);
-	void init_blocker(uint8_t sl, std::vector<uint64_t>& blocker);
-	void init_move(uint8_t sl, const std::vector<uint64_t>& blocker, std::vector<uint64_t>& attack_temp);
-	void init_magic(uint8_t sl, const std::vector<uint64_t>& blocker, const std::vector<uint64_t>& attack_temp);
-	void init_connect(uint8_t sl, const std::vector<uint64_t>& blocker, const std::vector<uint64_t>& attack_temp);
+	void init_magic();
+	void init_magic(int sl, const std::vector<uint64_t>& blocker, const std::vector<uint64_t>& attack_temp);
+	void init_mask(int sl);
+	void init_move(int sl, const std::vector<uint64_t>& blocker, std::vector<uint64_t>& attack_temp);
+	void init_ray(int sl);
+	void init_seeds();
 }
+
+namespace attack
+{
+	uint64_t by_pawns(const board& pos, int col);
+}
+
+inline void real_shift(uint64_t& bb, const int shift)
+{
+	bb = bb << shift | bb >> (64 - shift);
+}
+
+constexpr magic::pattern ray[]
+{
+	{8, 0xff00000000000000}, {7, 0xff01010101010101},
+	{63, 0x0101010101010101}, {55, 0x01010101010101ff},
+	{56, 0x00000000000000ff}, {57, 0x80808080808080ff},
+	{1, 0x8080808080808080}, {9, 0xff80808080808080}
+};
+
+constexpr int cap_left[]{9, 55, -9};
+constexpr int cap_right[]{7, 57, -7};
+constexpr int double_push[]{16, 48, -16};
+constexpr int push[]{8, 56, -8};
+constexpr int table_size = 107648;
+
+constexpr uint64_t border[]{file[fa], file[fh]};
+constexpr uint64_t promo_rank = rank[r1] | rank[r8];
+constexpr uint64_t third_rank[]{rank[r3], rank[r6]};
+
+inline int pin_cnt;
+inline magic::entry slider[2][64];
+inline std::vector<uint64_t> attack_table;
+
+inline uint32_t pin_idx[8];
+inline uint64_t border_left;
+inline uint64_t border_right;
+inline uint64_t enemies;
+inline uint64_t fr_king;
+inline uint64_t friends;
+inline uint64_t gen_type[2];
+inline uint64_t pawn_rank;
+inline uint64_t slide_ray[2][64];
+inline uint8_t ahead;
+inline uint8_t back;
 
